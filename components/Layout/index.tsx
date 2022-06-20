f5e1443d9ee2c2162e7bf7db0bf8e197741a4e58
Luis Martinez <malbertoa_11@hotmail.com>
2022-06-19 22:22:49 -0400

    Add footer

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import { Container } from 'react-bulma-components';
+import { Container, Content, Footer } from 'react-bulma-components';
 import Navbar from './Navbar';
 
 interface LayoutProps {
@@ -12,6 +12,11 @@ function Layout({ children }: LayoutProps) {
       <Container style={{ marginTop: 30 }}>
         { children }
       </Container>
+      <Footer style={{ marginTop: 20 }}>
+        <Content textAlign="center">
+          Built by Luis using Covalent
+        </Content>
+      </Footer>
     </div>
   );
 }
